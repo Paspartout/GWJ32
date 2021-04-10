e66df7b4d988326fb187871fb1555910b31670e3
Paspartout <2720369+Paspartout@users.noreply.github.com>
2021-04-10 10:57:43 +0200

    Initial commit

---
+++ PathSpawner.gd
@@ -0,0 +1,12 @@
+extends Path2D
+onready var time = $Timer
+var timer = 0
+export var spawnTime = 5
+ 
+var follower = preload("res://CharFollower.tscn")
+ 
+
+
+func _on_Timer_timeout() -> void:
+		var newFollower = follower.instance()
+		add_child(newFollower)
