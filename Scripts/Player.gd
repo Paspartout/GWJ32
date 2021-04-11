9733f14374c6304f881b4e151a4a1c1505df2bb8
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-04-11 13:10:34 +0100

    Made animations for the player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,28 +3,40 @@ extends KinematicBody2D
 var speed = 400
 var velocity : Vector2 = Vector2()
 var facingDirection : Vector2 = Vector2()
+var state_machine
+
+func _ready():
+	state_machine = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	
 	velocity = Vector2()
 	
 	if Input.is_action_pressed("up"):
+		state_machine.travel("RunBack")
 		velocity.y -= 1
 		facingDirection = Vector2(0, -1)
 		
 	if Input.is_action_pressed("down"):
+		state_machine.travel("RunFront")
 		velocity.y += 1
 		facingDirection = Vector2(0, 1)
 		
 	if Input.is_action_pressed("left"):
+		state_machine.travel("RunLeft")
 		velocity.x -= 1
 		facingDirection = Vector2(-1, 0)
 		
 	if Input.is_action_pressed("right"):
+		state_machine.travel("RunRight")
 		velocity.x += 1
 		facingDirection = Vector2(1, 0)
 	
+	if velocity.length() == 0:
+		state_machine.travel("IdleFront")
+		
+	
 	velocity = velocity.normalized()
 	
-	# move the player
+
 	move_and_slide(velocity * speed)
