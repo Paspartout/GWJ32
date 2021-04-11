7ee552258551b628a10815bfeedcc27f713203bf
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-04-11 14:05:44 +0100

    Made some foot particles

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,8 @@ var velocity : Vector2 = Vector2()
 var facingDirection : Vector2 = Vector2()
 var state_machine
 
+
+
 func _ready():
 	state_machine = $AnimationTree.get("parameters/playback")
 
@@ -13,27 +15,33 @@ func _physics_process(delta):
 	velocity = Vector2()
 	
 	if Input.is_action_pressed("up"):
+		
 		state_machine.travel("RunBack")
 		velocity.y -= 1
 		facingDirection = Vector2(0, -1)
+		$CPUParticles2D.emitting = true
 		
 	if Input.is_action_pressed("down"):
+		$CPUParticles2D.emitting = true
 		state_machine.travel("RunFront")
 		velocity.y += 1
 		facingDirection = Vector2(0, 1)
 		
 	if Input.is_action_pressed("left"):
+		$CPUParticles2D.emitting = true
 		state_machine.travel("RunLeft")
 		velocity.x -= 1
 		facingDirection = Vector2(-1, 0)
 		
 	if Input.is_action_pressed("right"):
+		$CPUParticles2D.emitting = true
 		state_machine.travel("RunRight")
 		velocity.x += 1
 		facingDirection = Vector2(1, 0)
 	
 	if velocity.length() == 0:
 		state_machine.travel("IdleFront")
+		$CPUParticles2D.emitting = false
 		
 	
 	velocity = velocity.normalized()
