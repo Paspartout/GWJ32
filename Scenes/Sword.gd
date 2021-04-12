d14bb04a29c2ee9c2892e85e0753c59c1e83b28f
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-04-12 16:38:55 +0100

    Sword Completed

--- Scenes/Sword.gd
+++ Scenes/Sword.gd
@@ -4,5 +4,10 @@ onready var enemies_list = get_tree().get_nodes_in_group("Enemy")
 
 
 func _on_Sword_area_entered(area: Area2D) -> void:
-	if area.get_parent() is Enemy:
-		area.hurt()
+	var enemy = area.get_parent() 
+	if enemy is Enemy:
+		enemy.hurt(1)
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("Attack"):
+		print("being pressed")
+		monitoring = !monitoring
