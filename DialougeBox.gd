7b15a695c48c6b2dd076843196371572ede183c9
Paspartout <2720369+Paspartout@users.noreply.github.com>
2021-04-15 16:58:32 +0200

    Make Dialogbox use Theme, move to HUD, fix weapon hitbox

--- DialougeBox.gd
+++ DialougeBox.gd
@@ -12,32 +12,31 @@ var dialog = [
 var dialog_index = 0 
 var finished = false
 
+onready var text_label = $Panel/RichTextLabel
+onready var tween = $Tween
+onready var indicator: Sprite = $Panel/Indicator
+
 func _ready():
 	load_dialog()
-	
 
 func _process(delta):
-	$Indicator.visible = finished
+	indicator.visible = finished
 	if Input.is_action_just_pressed("ui_accept"):
 		load_dialog()
 
 func load_dialog():
 	if dialog_index < dialog.size():
 		finished = false
-		$RichTextLabel.bbcode_text = dialog[dialog_index]
-		$RichTextLabel.percent_visible = 0
-		$Tween.interpolate_property(
-			$RichTextLabel, "percent_visible", 0 , 1 , 1 ,
+		text_label.bbcode_text = dialog[dialog_index]
+		text_label.percent_visible = 0
+		tween.interpolate_property(
+			text_label, "percent_visible", 0 , 1 , 1 ,
 			Tween.TRANS_LINEAR , Tween.EASE_IN_OUT
 		)
-		$Tween.start()
+		tween.start()
 	else:
 		queue_free()
 	dialog_index += 1
-
-
-
-
 
 func _on_Tween_tween_completed(object: Object, key: NodePath) -> void:
 	finished = true
