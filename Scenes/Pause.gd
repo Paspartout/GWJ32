0bae091dbf1d2b9bc3694dbef27ea734858af88f
Paspartout <2720369+Paspartout@users.noreply.github.com>
2021-04-18 18:53:50 +0200

    Add main menu background and music, close windows by escape

--- Scenes/Pause.gd
+++ Scenes/Pause.gd
@@ -1,10 +1,12 @@
 extends Control
 
+onready var game: Game = get_tree().root.get_node("Game")
+
 func _ready():
 	set_visible(false)
 
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") and not game.tower_menu.visible:
 		set_visible(!get_tree().paused) 
 		get_tree().paused = !get_tree().paused 
 
@@ -20,7 +22,6 @@ func _on_Menu_pressed():
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
 	get_tree().paused = false
 	set_visible(false)
-
 
 func _on_Fullscreen_pressed() -> void:
 	get_tree().paused = false
